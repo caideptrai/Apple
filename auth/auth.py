b752ac83af04e42e2b7341674ec40b36d6d83066
Lê Văn Hùng <levanhung11122003@gmail.com>
2024-04-20 19:37:51 +0700

    change logic auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -1,41 +1,55 @@
-# from flask import Blueprint, render_template
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from models import User, db
-
 
 auth_blueprint = Blueprint('auth', __name__, template_folder='templates')
 
 @auth_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        # Lấy thông tin từ form
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        confirm_password = request.form.get('confirm-password')
-        name = request.form.get('name')  # Thêm dòng này để lấy giá trị tên từ form
+        name = request.form.get('name')
+        phone_number = request.form.get('phone_number')
+        budget = request.form.get('budget')
 
-        # Kiểm tra xác nhận mật khẩu và kiểm tra email đã tồn tại
-        if password != confirm_password:
-            flash('Passwords do not match!', 'error')
-            return redirect(url_for('auth.register'))
+        # Kiểm tra xem có bất kỳ trường nào trống không
+        if not (username and email and password and name and phone_number and budget):
+            error_message = 'Please fill in all fields.'
+            return render_template('register.html', error_message=error_message)
 
+        # Kiểm tra xem email đã được sử dụng chưa
         existing_user = User.query.filter_by(email=email).first()
         if existing_user:
-            flash('Email already exists. Please use a different email.', 'error')
-            return redirect(url_for('auth.register'))
+            error_message = 'Email already exists. Please use a different email.'
+            return render_template('register.html', error_message=error_message)
 
-        # Tạo một hash password và tạo một instance của User
+        # Tạo hash password
         hashed_password = generate_password_hash(password)
-        new_user = User(username=username, email=email, hash_password=hashed_password, name=name)  # Thêm name vào đây
 
-        # Thêm user mới vào cơ sở dữ liệu và chuyển hướng người dùng đến trang đăng nhập
+        # Tạo user mới
+        new_user = User(
+            username=username,
+            email=email,
+            hash_password=hashed_password,
+            role='customer',
+            name=name,
+            phone_number=phone_number,
+            budget=budget
+        )
+
+        # Thêm user mới vào cơ sở dữ liệu
         db.session.add(new_user)
         db.session.commit()
-        flash('Registered successfully. You can now log in.', 'success')
+
+        # Đăng ký thành công, chuyển hướng đến trang đăng nhập
         return redirect(url_for('auth.login'))
 
     return render_template('register.html')
+
+
 
 @auth_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
--- general/general.py
+++ general/general.py
@@ -10,37 +10,37 @@ def index():
     game_hero_section = Games.query.filter_by(name='Rocket League').first()
     games_best_seller = Games.query.order_by(Games.purchase_number).limit(10).all()
     games_by_price = Games.query.order_by(Games.price).all()
-    # games_recommended = []
-    # pc = Platform.query.filter_by(platform_name='PC').first()
-    curr_user = User.query.filter_by(id=2).first()
-    session['current_user'] = []
-    cart_length = 0
-    if curr_user:
-        orders = Orders.query.filter_by(customer_id = curr_user.id).all()
-        cart_length = len(orders)
-        session['subtotal'] = 0
-        session['cart'] = []
-        for order in orders:
-            game_order_obj = {
-                'image': order.game.image,
-                'name': order.game.name,
-                'price': order.game.price
-            }
-            session['cart'].append(game_order_obj)
-            session['subtotal'] += order.game.price
-            
-        user_obj = {
-            'username': curr_user.username,
-            'name': curr_user.name,
-            'budget': curr_user.budget
+
+    # Kiểm tra xem có session người dùng nào đang tồn tại hay không
+    if 'user_id' not in session:
+        # Nếu không, đăng nhập với người dùng có id là 1
+        user = User.query.get(1)
+        session['user_id'] = user.id
+        session['current_user'] = {
+            'username': user.username,
+            'name': user.name,
+            'budget': user.budget
         }
-        session['current_user'] = user_obj
+
+    curr_user = User.query.get(session['user_id'])
+
+    cart_length = Orders.query.filter_by(customer_id=curr_user.id).count()
+    session['subtotal'] = 0
+    session['cart'] = []
+    for order in curr_user.orders:
+        game_order_obj = {
+            'image': order.game.image,
+            'name': order.game.name,
+            'price': order.game.price
+        }
+        session['cart'].append(game_order_obj)
+        session['subtotal'] += order.game.price
 
     session['cart_length'] = cart_length
+    session['current_user'] = {
+        'username': curr_user.username,
+        'name': curr_user.name,
+        'budget': curr_user.budget
+    }
 
-    # for game in games_by_price:
-    #     if pc in game.platforms_of_game:
-    #         games_recommended.append(game)
-            
-    # return render_template('general/index.html', games_best_seller=games_best_seller, games_recommended=games_recommended, game_hero_section=game_hero_section)
-    return render_template('general/index.html', games_best_seller = games_best_seller, game_hero_section=game_hero_section)
+    return render_template('general/index.html', games_best_seller=games_best_seller, game_hero_section=game_hero_section)
